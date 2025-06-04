bac8955661fd063abbd9168f76e83e80a14a3e87
jkbPolec <jkb.polec@gmail.com>
2025-06-04 17:07:53 +0200

    gitignore cleanup

--- WarsztatSamochodowyApp/obj/Debug/net9.0/WarsztatSamochodowyApp.AssemblyInfo.cs
+++ WarsztatSamochodowyApp/obj/Debug/net9.0/WarsztatSamochodowyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarsztatSamochodowyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd276a34e68f84605b4021038c4a43f9bec263f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarsztatSamochodowyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarsztatSamochodowyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
